c9663fb2e7f75cd20d092df83a41b6a0ab2aa48f
Matthew Lawler <raisty@gmail.com>
2021-09-15 13:20:28 +1000

    test bootstrap update

--- bootstrap/test.sh
+++ bootstrap/test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 # Install operator
-kustomize build operator
+kustomize build operator | oc apply -f -
+
+sleep 5;
+
+# The only install plans will be for the gitops operator as nothing else has been installed yet.
+installPlan=`oc get installPlans -n openshift-operators --no-headers -o name | head -1`
 
 # Wait on the operator installation
-
-# Install the configuration
-kustomize build config/overlay/test
+if oc wait -n openshift-operators ${installPlan} --for=condition=Installed --timeout=120s; then
+    # Install the configuration
+    kustomize build config/overlay/test | oc apply -f -
+else
+    echo "Failed to rollout gitops. Please check status before rerunning."
+fi
